464e10be6dc675cf96de66bdc4992baefd163800
jibin jose <jibinjose884@gmail.com>
2026-09-20 20:15:52 +0530

    Create hello_world.cpp to print Hello World
    Initial implementation of a C++ program that prints 'Hello, World!' to the console.

---
+++ hello_world.cpp
@@ -0,0 +1,24 @@
+// -----------------------------------------
+// File Name: hello_world.cpp
+// Description: C++ program to print Hello World
+// Author: Jibin Jose
+// -----------------------------------------
+
+// Running Command:
+// g++ hello_world.cpp -o hello_world
+// .\hello_world.exe
+
+// Output:
+// Hello, World!
+// -----------------------------------------
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
