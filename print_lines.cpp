7ecc28cd7f53ff34c962fde5b46c9acebef3f743
jibin jose <jibinjose884@gmail.com>
2026-09-20 20:17:42 +0530

    Add print_lines.cpp to output multiple lines
    This C++ program prints multiple lines of text to the console.

---
+++ print_lines.cpp
@@ -0,0 +1,28 @@
+// -----------------------------------------
+// File Name: print_lines.cpp
+// Description: C++ program to print multiple lines
+// Author: Jibin Jose
+// -----------------------------------------
+
+// Running Command:
+// g++ print_lines.cpp -o print_lines
+// .\print_lines.exe
+
+// Output:
+// My name is Jibin Jose.
+// I am learning C++.
+// C++ is a powerful programming language.
+// -----------------------------------------
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "My name is Jibin Jose." << endl;
+    cout << "I am learning C++." << endl;
+    cout << "C++ is a powerful programming language." << endl;
+
+    return 0;
+}
