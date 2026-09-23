e291445e4369ddbb2ead39ea9ac3bd4b9fd06e51
jibin jose <jibinjose884@gmail.com>
2026-09-23 22:04:43 +0530

    Add C++ program to demonstrate basic data types
    This program demonstrates the use of basic data types in C++ including int, double, char, and bool.

---
+++ data_types.cpp
@@ -0,0 +1,35 @@
+// -----------------------------------------
+// File Name: data_types.cpp
+// Description: C++ program demonstrating basic data types
+// Author: Jibin Jose
+// -----------------------------------------
+
+// Running Command:
+// g++ data_types.cpp -o data_types
+// .\data_types.exe
+
+// Output:
+// Age: 25
+// Height: 5.8
+// Grade: A
+// Student: 1
+// -----------------------------------------
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int age = 25;
+    double height = 5.8;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
