c296fea5880ed2cb9f5947f01ea2c9135f9f43f4
jibin jose <jibinjose884@gmail.com>
2026-09-23 22:01:50 +0530

    Add integer_variable.cpp to demonstrate integer usage
    This program demonstrates the use of an integer variable in C++ by printing the age.

---
+++ integer_variable.cpp
@@ -0,0 +1,26 @@
+// -----------------------------------------
+// File Name: integer_variable.cpp
+// Description: C++ program demonstrating an integer variable
+// Author: Jibin Jose
+// -----------------------------------------
+
+// Running Command:
+// g++ integer_variable.cpp -o integer_variable
+// .\integer_variable.exe
+
+// Output:
+// Age: 25
+// -----------------------------------------
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int age = 25;
+
+    cout << "Age: " << age << endl;
+
+    return 0;
+}
